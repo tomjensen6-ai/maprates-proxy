9ce47945c34710b7673c88bad39199aaebcb3f9b
Tom Jensen <tom.jensen6@gmail.com>
2025-10-14 16:59:18 +0200

    Add football-competitions endpoint

--- api/fg/football-competitions.js
+++ api/fg/football-competitions.js
@@ -7,14 +7,26 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
+  const { country, season, id } = req.query;
   const apiKey = process.env.FOOTBALL_API_KEY;
   
   if (!apiKey) {
     return res.status(500).json({ error: 'Missing FOOTBALL_API_KEY' });
   }
 
+  // Build query parameters
+  let url = 'https://v3.football.api-sports.io/leagues';
+  const params = new URLSearchParams();
+  if (country) params.append('country', country);
+  if (season) params.append('season', season);
+  if (id) params.append('id', id);
+  
+  if (params.toString()) {
+    url += `?${params.toString()}`;
+  }
+
   try {
-    const response = await fetch('https://v3.football.api-sports.io/leagues', {
+    const response = await fetch(url, {
       headers: { 'x-apisports-key': apiKey }
     });
     
