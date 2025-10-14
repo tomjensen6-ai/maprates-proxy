1d8e6adceb0464052aabdc8cdc8df2577f056ebd
Tom Jensen <tom.jensen6@gmail.com>
2025-10-14 17:50:42 +0200

    Fix runtime config for Vercel

--- api/fg/geocode.js
+++ api/fg/geocode.js
@@ -1,4 +1,4 @@
-export const config = { runtime: 'nodejs18.x' };
+export const config = { runtime: 'nodejs' };
 export default async function handler(req, res) {
   try {
     res.setHeader('Access-Control-Allow-Origin', '*');
